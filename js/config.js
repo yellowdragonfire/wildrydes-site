4712f2dfd870b05e563dee7acc901a093dcafba9
yellowdragonfire <144542551+yellowdragonfire@users.noreply.github.com>
2023-11-19 09:27:39 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-east-2_EeYOAaNQm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6q5v2pd1ntfv4ep9ofa44a4pcg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
